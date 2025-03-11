89e8db5da3a51895530274d8aa29fa66c5579260
mateotr240 <mateotr24033@gmail.com>
2025-03-11 00:21:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 int main()
@@ -7,7 +8,6 @@ int main()
     int dañototal;
     int daño;
     int a;
-    
     std::cout << "valor del Escudo"<<std::endl;
     std::cin >> escudo;  
     std::cout << "valor del daño"<<std::endl;
@@ -17,37 +17,55 @@ int main()
     //primera "condicion escudo es mayor a 70 y el ataque es de plasma (1)"//
     if((escudo>=70)&&(tipodeataque==1)){
         dañototal=daño/2+daño;
+        if(dañototal>=escudo)
+        //posibles respuestas//
+        {
+            escudo=0;
+        std::cout << "¡Sobre carga de escudo! daño aumentado" <<std::endl;
+        std::cout << "daño recibido: "<< dañototal<<std::endl;
+        std::cout << "escudo restante: " <<escudo<< std::endl;
+        std::cout << "¡Modo de ira activado! El siguiente ataque será más fuerte." <<std::endl;
+        }
+        else{
         std::cout << "¡Sobre carga de escudo! daño aumentado" <<std::endl;
         std::cout << "daño recibido: "<< dañototal<<std::endl;
         std::cout << "escudo restante: " <<escudo- dañototal << std::endl;
+    }
     }
     //segunda condicion "escudo es menor a 30 y el ataque es de fuego infernal (2)"//
     else if((escudo<=30)&&(tipodeataque==2)){
         dañototal=(daño -(daño*0.4));
         a=escudo - dañototal;
-    //primer posible tipo de respuesta (cuando el escudo restante sea menor o igual a 0)//
+    //posibles respuestas//
         if(a<=0){
             escudo=0;
             std::cout << "daño recibido: "<< dañototal<<std::endl;
             std::cout << "escudo restante: " <<escudo<< std::endl;
             std::cout << "¡Protección infernal activada! Daño reducido." <<std::endl;
             std::cout << "¡Modo de ira activado! El siguiente ataque será más fuerte." <<std::endl;
-        //segundo tipo de posible respuesta(cuando el escudo restante sea mayor a 0)//
         }
         else{
             std::cout << "daño recibido: "<<dañototal<<std::endl;
             std::cout << "escudo restante: " <<a<< std::endl;
             std::cout << "¡Protección infernal activada! Daño reducido." <<std::endl;
         }
-    //terceracondicion "Si el escudo se reduce a 0"//    
+    //tercera condicion "Si el escudo se reduce a 0"//    
     }else if(escudo<=0){
         std::cout << "¡Modo de ira activado! El siguiente ataque será más fuerte." <<std::endl;
         std::cout << "daño recibido: "<< daño<<std::endl;
         std::cout << "escudo restante: " <<escudo<< std::endl;
     }
-     //si ninguna de las condiciones anteriores se cumplen//
+    //si ninguna de las condiciones anteriores se cumplen//
     else{
+        //posibles respuestas//
+        if(escudo>=daño){
     std::cout << "daño recibido: "<< daño<<std::endl;
     std::cout << "escudo restante: " <<escudo-daño<< std::endl;
-    }    
+        }
+    else{
+        escudo=0;
+    std::cout << "daño recibido: "<< daño<<std::endl;
+    std::cout << "escudo restante: " <<escudo<< std::endl;
+    }
+    }
 }
